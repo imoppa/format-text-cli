8ba2147c44b9ee15e6b2de9655a3fd36aa3c4261
MadKimchi <minkim.join@gmail.com>
2022-01-25 00:53:42 -0500

    feat: add cli logic

--- src/main/java/com/formattextcli/app/App.java
+++ src/main/java/com/formattextcli/app/App.java
@@ -1,13 +1,30 @@
 package com.formattextcli.app;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
- * Hello world!
+ * FORMAT TEXT CLI APP Class
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
+    // TODO: read this from config file
+    private static String version = "1.0.0";
+
+    public static void main( String[] _args )
     {
-        System.out.println( "Hello World!" );
+        List<String> args = Arrays.asList(_args);
+
+        while (!args.isEmpty()) {
+            String firstCommand = args.get(0);
+
+            if (firstCommand.equals("-version")) {
+                System.out.println("Version: " + version);
+                return;
+            }
+
+            return;
+        }
     }
 }
