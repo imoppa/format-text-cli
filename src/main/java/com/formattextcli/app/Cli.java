9fe0863ae0e7287b80f86a946894812d31aa147a
MadKimchi <minkim.join@gmail.com>
2022-01-25 11:01:42 -0500

    add file read and default cli commands

--- src/main/java/com/formattextcli/app/Cli.java
+++ src/main/java/com/formattextcli/app/Cli.java
@@ -1,5 +1,9 @@
 package com.formattextcli.app;
 
+import java.io.*;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.List;
 
@@ -12,15 +16,56 @@ public class Cli {
     public void start(String[] _args) {
         List<String> args = Arrays.asList(_args);
 
-        while (!args.isEmpty()) {
-            String firstCommand = args.get(0);
-
-            if (firstCommand.equals("-version") || firstCommand.equals("-v")) {
-                System.out.println("Version: " + this._version);
-                return;
-            }
-
+        if (args.isEmpty()) {
+            System.out.println("Do you need some help to learn how to use this CLI tool?");
             return;
+        }
+
+        while (!args.isEmpty()) {
+            String firstCommand = args.get(0).toLowerCase();
+
+            switch (firstCommand) {
+                case "-v":
+                case "-version": {
+                    this._handleVersionCommand();
+                    return;
+                }
+                default: {
+                    Path filePath = Path.of(firstCommand);
+                    if (Files.exists(filePath)) {
+                        this._handleDefaultCommand(filePath);
+                        return;
+                    }
+
+                    System.out.println("It seems that it is an incorrect file path");
+                    return;
+                }
+            }
+        }
+    }
+
+    private void _handleVersionCommand() {
+        System.out.println("Version: " + this._version);
+    }
+
+    private void _handleDefaultCommand(Path filePath) {
+        URI uri = filePath.toUri();
+
+        try {
+            File inputFile = new File(uri);
+            InputStream inputFileStream = new BufferedInputStream(new FileInputStream(inputFile));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputFileStream));
+
+            String line = reader.readLine();
+
+            long linesWritten = 0;
+            while (line != null) {
+                line = reader.readLine();
+                System.out.println(line);
+                linesWritten++;
+            }
+        } catch (Exception e) {
+            System.err.println("Something went wrong!");
         }
     }
 }
